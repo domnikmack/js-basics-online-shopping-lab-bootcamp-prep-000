786933205a5f336f98c9d6589e641344af2a7592
Domnik Mack <domnikmack@gmail.com>
2017-09-27 21:56:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function total() {
 function removeFromCart(item) {
   for (var i=0; i < cart.length; i++) {
     if (!cart[i].hasOwnProperty(item)) {
-    console.log('That item is not in your cart.');
+      console.log('That item is not in your cart.');
     } else {
     var index = cart.indexOf(item);
     cart.splice(index, 1);
