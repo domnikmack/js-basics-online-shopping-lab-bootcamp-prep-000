7e75743bbd8afd2acc7f907a339ded19fc7edd81
Domnik Mack <domnikmack@gmail.com>
2017-09-28 00:01:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,15 +43,15 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (var i=0; i < cart.length; i++) {
-    if (!cart[i].hasOwnProperty(item)) {
-      console.log('That item is not in your cart.');
-    } else {
-    var index = cart[i].indexOf(item);
-    cart.splice(index, 1);
+	for (var i = 0; i < cart.length; i++) {
+  		if (cart[i].hasOwnProperty(item)) {
+    		cart.splice(i, 1);
+        } else {
+    		console.log('That item is not in your cart.');
+ 		}
+	
     }
-  }
-    return cart;
+	return cart;
 }
 
 function placeOrder(cardNumber) {
